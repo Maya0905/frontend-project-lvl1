6c1d0c0505961e5af4f47002d644cdce20dd4de9
Maya <mayyaklych@gmail.com>
2022-05-10 18:20:10 +1000

    add eslint/make lint/github-actions

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readLineSync from 'readline-sync';
 
 const greeting = () => {
-   const name = readLineSync.question('May I have your name?: ');  
-    console.log(`Hello, ${name}!`);
+  const name = readLineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
 };
 export default greeting;
